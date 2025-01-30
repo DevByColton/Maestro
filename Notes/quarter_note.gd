40bb4751814f7f59f79a79651d2ce62a3d240192
smothergill <chrismschulze@gmail.com>
2025-01-30 10:32:18 -0600

    movement

---
+++ Notes/quarter_note.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var toward_player = false
+@export var toward_boss = false
+@export var test = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if toward_player:
+		move_toward_player()
+
+func move_toward_player():
+	self.position.x -= 1
+
+func move_toward_boss():
+	self.position.x += 1
