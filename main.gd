01a819de7f54e947d29fc5964721a38e55cef8f4
Colton Morales <developmentbycolton@outlook.com>
2025-02-01 11:17:29 -0600

    Got platforming working

--- main.gd
+++ main.gd
@@ -1,58 +1,43 @@
 extends Node
-@export var notes = []
-@export var current_note = 0
+
+@export var quarter_note_w_scene: PackedScene
+@export var quarter_note_q_scene: PackedScene
+@export var half_note_scene: PackedScene
+@export var dotted_half_note_scene: PackedScene
+@export var whole_note_scene: PackedScene
+
+var wns
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$NoteTimer.start()
-	var quarter_note_q = load("res://Notes/quarter_note_q.tscn")
-	var quarter_note_w = load("res://Notes/quarter_note_w.tscn")
-	var half_note = load("res://Notes/half_note.tscn")
-	var dotted_half = load("res://Notes/dotted_half_note.tscn")
-	var whole_note = load("res://Notes/whole_note.tscn")
 	
-	var instances = [
-		quarter_note_w.instantiate(),
-		quarter_note_q.instantiate(),
-		half_note.instantiate(),
-		dotted_half.instantiate(),
-		whole_note.instantiate(),
-		quarter_note_w.instantiate(),
-		half_note.instantiate(),
-		dotted_half.instantiate(),
-		whole_note.instantiate(),
-		quarter_note_w.instantiate(),
-		half_note.instantiate(),
-		dotted_half.instantiate(),
-		whole_note.instantiate(),
-		quarter_note_w.instantiate(),
-		half_note.instantiate(),
-		dotted_half.instantiate(),
-		whole_note.instantiate(),
-		quarter_note_w.instantiate(),
-		half_note.instantiate(),
-		dotted_half.instantiate(),
-		whole_note.instantiate(),
-	]
+	var qnw = quarter_note_w_scene.instantiate()
+	qnw.position = Vector2(100, 100)
+	add_child(qnw)
 	
-	for instance in instances:
-		add_child(instance)
-		notes.append(instance)
-
+	var qnq = quarter_note_q_scene.instantiate()
+	qnq.position = Vector2(400, 560)
+	add_child(qnq)
+	
+	var hns = half_note_scene.instantiate()
+	hns.position = Vector2(500, 460)
+	add_child(hns)
+	
+	var dhns = dotted_half_note_scene.instantiate()
+	dhns.position = Vector2(600, 360)
+	add_child(dhns)
+	
+	wns = whole_note_scene.instantiate()
+	wns.position = Vector2(700, 260)
+	add_child(wns)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	#wns.position.x -= 100 * delta
+	
 	if Input.is_action_just_pressed("play_song"):
 		if $GameplaySong.playing:
 			$GameplaySong.stop()
 		else:
 			$GameplaySong.play()
-
-
-func _on_note_timer_timeout() -> void:
-	if current_note < notes.size() - 1:
-		current_note += 1
-	else:
-		current_note = 0
-	
-	notes[current_note].toward_player = true
